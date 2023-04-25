d9a5eb7e39c0502df85c87ac1af8bbe71bebd5b6
diego <goethalsdiego@gmail.com>
2023-04-26 01:27:30 +0200

    Made the basic of a clickable card to go get the 'projects' of a certain theme which will then display another list of all 'projects' which will all have their own to do's

---
+++ src/Theme.js
@@ -0,0 +1,11 @@
+import './Theme.css';
+
+function Theme() {
+    const theme = "Programming projects";
+
+    return (
+        <li className="theme">{ theme }</li>
+    );
+}
+
+export default Theme;
