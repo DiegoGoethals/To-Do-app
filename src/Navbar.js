dfe8767f75556af3700402380a6f2184caeeb039
diego <goethalsdiego@gmail.com>
2023-04-25 22:22:30 +0200

    Reworked styling a very little bit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <h1>To Do</h1>
+      <div className='links'>
+        <a href='/'>Home</a>
+        <a href='/ideas'>Ideas</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
