840c2bd32549693a8354cc95612b70db89862834
diego <goethalsdiego@gmail.com>
2023-04-27 23:58:51 +0200

    Added nice icons to see if a to do is done or not

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,12 +2,15 @@ import './Category.css';
 
 function ToDo(props) {
     const item = props.item;
+    let done = <i class="fa-solid fa-circle-xmark" style={{"color": "#d60000"}}></i>
+    if (item.done) {
+        done = <i class="fa-sharp fa-solid fa-circle-check" style={{"color": "#47e006"}}></i>;
+    }
 
     return (
         <li className="item">
             <div>
-                <h3>{item.todo}</h3>
-                <h3>{item.done.toString()}</h3>
+                <h3>{done} {item.todo}</h3>
             </div>
          </li>
     )
