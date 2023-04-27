81965c8630c4e0df252a9ac86068023d736877c9
diego <goethalsdiego@gmail.com>
2023-04-28 01:44:00 +0200

    Made it possible to add entire to do lists

--- src/ListsItem.js
+++ src/ListsItem.js
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 import './Category.css';
+import deleteList from "./firebase/firebase_handlers/deleteList";
 
 function ListsItem(props) {
     const item = props.item;
     const totalItems = item.items.length;
     const doneItems = item.items.filter(item => item.done).length;
+
+    const onClickHandler = () => {
+        deleteList(item.id);
+    }
 
     return (
         <Link to={item.name}>
@@ -13,7 +18,11 @@ function ListsItem(props) {
                     <h3>{item.name}</h3>
                     <p>{item.description}</p>
                     <p>{doneItems} of {totalItems} done</p>
-                    <i className="fa-solid fa-trash-can" style={{"color": "#7c7e83"}}></i>
+                    <i className="fa-solid fa-trash-can" style={{"color": "#7c7e83"}} onClick={(e) => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        onClickHandler();
+                    }}></i>
                 </div>
             </li>
         </Link>
---
+++ src/firebase/firebase_handlers/deleteList.js
@@ -0,0 +1,13 @@
+import { deleteDoc, doc } from "@firebase/firestore"
+import { firestore } from "../firebase_setup/firebase"
+
+function deleteList(id) {
+    const docRef = doc(firestore, "to do lists", id);
+    try {
+        return deleteDoc(docRef);
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+export default deleteList;
