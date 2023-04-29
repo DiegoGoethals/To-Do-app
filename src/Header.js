c8a24810cc2998358d1d3b08a09a78854061dc3b
diego <goethalsdiego@gmail.com>
2023-04-29 17:31:42 +0200

    Added light theme and a switch button, now need to improve styling of the button

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
+import { useEffect, useState } from 'react';
 import { auth } from './firebase/firebase_setup/firebase';
 
 function Header(props) {
@@ -12,6 +13,27 @@ function Header(props) {
     });
   };
 
+  const [theme, setTheme] = useState('dark');
+
+  const root = document.querySelector(':root');
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+      root.style.setProperty('--bg-color', 'rgb(48,48,48)');
+      root.style.setProperty('--color', 'rgb(178,22,77)');
+    } else {
+      setTheme('light');
+      root.style.setProperty('--bg-color', 'whitesmoke');
+      root.style.setProperty('--color', 'black');
+    }
+  };
+
+  useEffect(() => {
+    console.log('theme changed');
+        document.body.className = theme;
+  }, [theme]);
+
   const userLoaded = props.userLoaded;
   const setUserLoaded = props.setUserLoaded;
 
@@ -21,6 +43,7 @@ function Header(props) {
       {!userLoaded && <p>Loading...</p>}
       {userLoaded && !auth.currentUser && <Link to="/login"><p>Log in</p></Link>}
       {userLoaded && auth.currentUser && <p onClick={onClickHandler} style={{"cursor": "pointer"}}>Log out</p>}
+      <p onClick={toggleTheme} style={{"cursor": "pointer"}}>Toggle theme</p>
     </div>
   );
 }
